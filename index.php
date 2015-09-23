dc94c380c3bb96955da03903fff170a23c39fcb4
polhec <tine.dolzan@gmail.com>
2015-09-23 15:27:45 +0200

    With template

--- index.php
+++ index.php
@@ -122,28 +122,11 @@ function related_posts()
 
 function rp_izpis()
 {
-	?>
-		<div class="box span span-2_1 fak">
-			<a href="<?php the_permalink() ?>" class="link-holder">
-				<div class="box-top">
-					<div class="meta">
-						<span class="date"><?php the_date() ?></span>
-						<h2 class="title"><?php the_title() ?></h2>
-					</div>
-							<?php
-							if( has_post_thumbnail() ):
-								the_post_thumbnail('category-grid',array("alt" => overnet_get_featured_img_alt(get_the_ID()))); 
-							else: 
-							?>                           
-							<img src="<?php echo get_theme_mod('overnet_default_featured_image'); ?>" alt="generic image">
-							<?php endif; ?>
-				</div>
-				<div class="box-bottom">
-						<?php the_excerpt() ?>
-				</div>
-			</a>
-		</div>
-	<?php
+
+	if ( !assert( "locate_template( 'rp_template.php', true, false )" ) )
+	{
+		include "rp_template.php";
+	}
 }
 
 ?>
